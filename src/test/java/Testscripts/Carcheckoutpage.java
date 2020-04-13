9d8158bfb5e605c74f98e52ef50e9609451a6252
Narendra Kulkarni <narendra0035@gmail.com>
2020-04-13 11:26:27 +0530

    commit on time

--- src/test/java/Testscripts/Carcheckoutpage.java
+++ src/test/java/Testscripts/Carcheckoutpage.java
@@ -21,7 +21,7 @@ public class Carcheckoutpage extends Testbase{
 		car.venueclick();
 		Thread.sleep(5000);
 		findelementandclick(clickonbikesorcar);
-		Thread.sleep(6000);
+		Thread.sleep(5000);
 		
 	
 }
